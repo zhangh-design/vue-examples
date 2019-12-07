9d7334bdf5d994c5400d20db8d4b6326f6da2ff6
zhanghao <506365426@qq.com>
2019-12-07 10:30:23 +0800

    feat : 示例demo

---
+++ 08 如何触发组件的更新/v-demo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <change-data-view></change-data-view>
+  </div>
+</template>
+
+<script>
+import ChangeDataView from './components/change/index.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ChangeDataView
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
