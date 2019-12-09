132b10e50bbc7363e6349d00cc1c81775f51b955
zhanghao <506365426@qq.com>
2019-12-09 17:46:40 +0800

    feat : 添加功能展示demo

---
+++ 09 合理应用计算属性和侦听器/vue-demo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <watch-index></watch-index>
+  </div>
+</template>
+
+<script>
+import WatchIndex from './components/watch/index.vue'
+
+export default {
+  name: 'app',
+  components: {
+    WatchIndex
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ 09 合理应用计算属性和侦听器/vue-demo/src/components/watch/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <computed />
+    <watch></watch>
+    <br/>
+    <Computed1></Computed1>
+    <watch1></watch1>
+  </div>
+</template>
+<script>
+import Computed from './Computed';
+import Computed1 from './Computed1';
+import Watch from './Watch';
+import Watch1 from './Watch1';
+/* import WatchPro from './Watch1_pro'; */
+export default {
+  components: {
+    Computed,
+    Computed1,
+    Watch,
+    Watch1
+    // WatchPro
+  }
+};
+</script>
