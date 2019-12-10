61704cd42c8d0d079543dfe252c3c0636e5ce458
zhangh <506365426@qq.com>
2019-12-10 22:58:06 +0800

    feat : 项目示例

---
+++ 10 生命周期的应用场景和函数式组件/vue-demo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <hook-index></hook-index>
+  </div>
+</template>
+
+<script>
+import HookIndex from './components/hook/index.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HookIndex
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ 10 生命周期的应用场景和函数式组件/vue-demo/src/components/hook/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <button @click="destroyClock = !destroyClock">
+          {{ destroyClock ? "加载时钟" : "销毁时钟" }}
+      </button>
+      <Clock v-if="!destroyClock" />
+      <Functional :name="name" />
+      <TempVar
+        :var1="`hello ${name}`"
+        :var2="destroyClock ? 'hello vue' : 'hello world'"
+      >
+        <template v-slot="{ var1, var2 }">
+          {{ var1 }}
+          {{ var2 }}
+        </template>
+      </TempVar>
+  </div>
+</template>
+<script>
+import Clock from './Clock'
+import Functional from './Functional'
+import TempVar from './TempVar'
+export default {
+  components: {
+    Clock,
+    Functional,
+    TempVar
+  },
+  data () {
+    return {
+      destroyClock: false,
+      name: 'vue'
+    }
+  }
+}
+</script>
