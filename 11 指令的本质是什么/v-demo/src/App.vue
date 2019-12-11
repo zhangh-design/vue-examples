ab882d53d8d63be0b0743924a9963d312c756c9b
zhangh <506365426@qq.com>
2019-12-11 23:11:22 +0800

    feat : demo工程上次

---
+++ 11 指令的本质是什么/v-demo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <instruct-index></instruct-index>
+  </div>
+</template>
+
+<script>
+import InstructIndex from './components/instruct/index.vue'
+
+export default {
+  name: 'app',
+  components: {
+    InstructIndex
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ 11 指令的本质是什么/v-demo/src/components/instruct/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- <Directives /> -->
+    <CustomerDirectives />
+  </div>
+</template>
+<script>
+// import Directives from './Directives'
+import CustomerDirectives from './CustomerDirectives'
+export default {
+  components: {
+    // Directives
+    CustomerDirectives
+  }
+}
+</script>
