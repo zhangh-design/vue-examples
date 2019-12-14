62ec54f5f32bbf076148c8e45767cc19f2da5e5c
zhangh <506365426@qq.com>
2019-12-14 22:51:13 +0800

    feat : 示例代码

---
+++ 12 常用高级特性provide、inject/v-demo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <provide-index></provide-index>
+  </div>
+</template>
+
+<script>
+import ProvideIndex from './components/provide/Index.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ProvideIndex
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
