c12252fe6eb6dda0b36f992d0bfcd15caf495e99
zhanghao <506365426@qq.com>
2019-12-23 18:28:34 +0800

    feat: demo

---
+++ 16 如何在Vue中使用Vuex/v-demo/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{ count }}
+    <br />
+    {{ $store.getters.doubleCount }}
+    <button @click="$store.commit('increment', 2)">count ++</button>&nbsp;
+    <button @click="$store.dispatch('increment')">count ++</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  }
+}
+</script>
+<style>
+
+</style>
