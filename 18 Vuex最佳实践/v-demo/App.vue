1f098d4c9231b71252f7b27e8a987ff81c7d68f5
zhanghao <506365426@qq.com>
2020-01-02 15:15:57 +0800

    chore: 主功能示例

---
+++ 18 Vuex最佳实践/v-demo/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div>
+      <h1>购物车示例</h1>
+      <p>账号: {{ email }}</p>
+      <hr />
+      <h2>产品</h2>
+      <ProductList />
+      <hr />
+      <ShoppingCart />
+    </div>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+import ProductList from './components/ProductList.vue'
+import ShoppingCart from './components/ShoppingCart.vue'
+export default {
+  components: {
+    ProductList,
+    ShoppingCart
+  },
+  computed: {
+    ...mapState({
+      email: state => state.userInfo.email
+    }),
+    count () {
+      return this.$store.state.count
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+<style></style>
